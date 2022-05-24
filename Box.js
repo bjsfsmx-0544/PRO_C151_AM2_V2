5564245e5f9e7b8572b931be8887cd48ae02e451
bjsfsmx-0544 <94998868+bjsfsmx-0544@users.noreply.github.com>
2022-05-24 14:27:38 -0500

    Traducción 1.00

--- Box.js
+++ Box.js
@@ -1,4 +1,4 @@
-// Registering component in box-component.js
+// Registrando el componente en box-component.js
 AFRAME.registerComponent("move-box", {
   schema: {
     moveX: { type: "number", default: 1 },
